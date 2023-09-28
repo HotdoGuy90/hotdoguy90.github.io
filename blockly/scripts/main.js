c463460c99d3b8d856ff303b534d249027996e33
HotdoGuy90 <143679952+HotdoGuy90@users.noreply.github.com>
2023-09-27 21:23:42 -0700

    Add files via upload

---
+++ blockly/scripts/main.js
@@ -0,0 +1,287 @@
+const toolbox = {
+    "kind": "categoryToolbox",
+    "contents": [
+        {
+            "kind": "category",
+            "name": "Lists",
+            "categorystyle": "list_category",
+            "contents": [
+                {
+                    "kind": "block",
+                    "type": "lists_create_empty"
+                },
+                {
+                    "kind": "block",
+                    "type": "lists_append"
+                },
+                {
+                    "kind": "block",
+                    "type": "lists_repeat"
+                },
+                {
+                    "kind": "block",
+                    "type": "lists_reverse"
+                },
+                {
+                    "kind": "block",
+                    "type": "lists_isEmpty"
+                },
+                {
+                    "kind": "block",
+                    "type": "lists_length"
+                }
+            ]
+        },
+        {
+            "kind": "category",
+            "name": "Colours",
+            "categorystyle": "colour_category",
+            "contents": [
+                {
+                    "kind": "block",
+                    "type": "colour_picker"
+                },
+                {
+                    "kind": "block",
+                    "type": "colour_random"
+                },
+                {
+                    "kind": "block",
+                    "type": "colour_rgb"
+                },
+                {
+                    "kind": "block",
+                    "type": "colour_blend"
+                }
+            ]
+        },
+        {
+            "kind": "category",
+            "name": "Loops",
+            "categorystyle": "loop_category",
+            "contents": [
+                {
+                    "kind": "block",
+                    "type": "controls_repeat_ext"
+                },
+                {
+                    "kind": "block",
+                    "type": "controls_repeat"
+                },
+                {
+                    "kind": "block",
+                    "type": "controls_whileUntil"
+                },
+                {
+                    "kind": "block",
+                    "type": "controls_for"
+                },
+                {
+                    "kind": "block",
+                    "type": "controls_forEach"
+                },
+                {
+                    "kind": "block",
+                    "type": "controls_flow_statements"
+                }
+            ]
+        },
+        {
+            "kind": "category",
+            "name": "Logic",
+            "categorystyle": "logic_category",
+            "contents": [
+                {
+                    "kind": "block",
+                    "type": "controls_if"
+                },
+                {
+                    "kind": "block",
+                    "type": "controls_ifelse"
+                },
+                {
+                    "kind": "block",
+                    "type": "logic_boolean"
+                },
+                {
+                    "kind": "block",
+                    "type": "logic_compare"
+                },
+                {
+                    "kind": "block",
+                    "type": "logic_operation"
+                },
+                {
+                    "kind": "block",
+                    "type": "logic_negate"
+                },
+                {
+                    "kind": "block",
+                    "type": "logic_null"
+                }
+            ]
+        },
+        {
+            "kind": "category",
+            "name": "Math",
+            "categorystyle": "math_category",
+            "contents": [
+                {
+                    "kind": "block",
+                    "type": "math_arithmetic"
+                },
+                {
+                    "kind": "block",
+                    "type": "math_number"
+                },
+                {
+                    "kind": "block",
+                    "type": "math_single"
+                },
+                {
+                    "kind": "block",
+                    "type": "math_trig"
+                },
+                {
+                    "kind": "block",
+                    "type": "math_constant"
+                },
+                {
+                    "kind": "block",
+                    "type": "math_number_property"
+                },
+                {
+                    "kind": "block",
+                    "type": "math_round"
+                },
+                {
+                    "kind": "block",
+                    "type": "math_on_list"
+                },
+                {
+                    "kind": "block",
+                    "type": "math_modulo"
+                },
+                {
+                    "kind": "block",
+                    "type": "math_constrain"
+                },
+                {
+                    "kind": "block",
+                    "type": "math_random_int"
+                },
+                {
+                    "kind": "block",
+                    "type": "math_random_float"
+                },
+                {
+                    "kind": "block",
+                    "type": "math_atan2"
+                }
+            ]
+        },
+        {
+            "kind": "category",
+            "name": "Text",
+            "categorystyle": "text_category",
+            "contents": [
+                {
+                    "kind": "block",
+                    "type": "text"
+                },
+                {
+                    "kind": "block",
+                    "type": "text_multiline"
+                },
+                {
+                    "kind": "block",
+                    "type": "text_print"
+                },
+                {
+                    "kind": "block",
+                    "type": "text_prompt"
+                },
+                {
+                    "kind": "block",
+                    "type": "text_join"
+                },
+                {
+                    "kind": "block",
+                    "type": "text_append"
+                },
+                {
+                    "kind": "block",
+                    "type": "text_length"
+                },
+                {
+                    "kind": "block",
+                    "type": "text_isEmpty"
+                },
+                {
+                    "kind": "block",
+                    "type": "text_indexOf"
+                },
+                {
+                    "kind": "block",
+                    "type": "text_copy"
+                },
+                {
+                    "kind": "block",
+                    "type": "text_paste_function"
+                },
+                {
+                    "kind": "block",
+                    "type": "text_paste"
+                }
+            ]
+        },
+        {
+            "kind": "sep"
+        },
+        {
+            "kind": "category",
+            "name": "Variables",
+            "custom": "VARIABLE",
+            "categorystyle": "variable_category",
+        },
+        {
+            "kind": "category",
+            "name": "Functions",
+            "custom": "PROCEDURE",
+            "categorystyle": "procedure_category"
+        },
+        {
+            "kind": "category",
+            "name": "Base64",
+            "colour": 30,
+            "contents": [
+                {
+                    "kind": "block",
+                    "type": "base64_encode"
+                },
+                {
+                    "kind": "block",
+                    "type": "base64_decode"
+                },
+                {
+                    "kind": "block",
+                    "type": "comment"
+                }
+            ]
+        },
+    ]
+}
+
+const workspace = Blockly.inject('blocklyId', {toolbox: toolbox})
+
+function generateCode(event) {
+    const jsCode = Blockly.JavaScript.workspaceToCode(workspace)
+    document.getElementById('codeOutput').getElementsByTagName('code')[0].innerHTML = jsCode
+}
+
+workspace.addChangeListener(generateCode)
+
+function runCode() {
+    const code = Blockly.JavaScript.workspaceToCode(workspace)
+    eval(code)
+}
