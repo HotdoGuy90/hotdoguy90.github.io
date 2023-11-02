47e7decb85442a0962c87903c285168cd6600f91
HotdoGuy90 <143679952+HotdoGuy90@users.noreply.github.com>
2023-11-01 18:30:56 -0700

    Update gm

---
+++ i-dont-know-games/scripts/project/scriptsInEvents.js
@@ -0,0 +1,28 @@
+
+
+
+const scriptsInEvents = {
+
+	async EventSheet1_Event19_Act1(runtime, localVars)
+	{
+		window.alert("You Have Completed Beta!");
+		window.alert("I will add more stuff in the future.");
+		window.alert("Like new levels and features!");
+		window.alert("But for now just hang tight.");
+		window.alert("Grab some popcorn.");
+		window.alert("And enjoy the fun!");
+		window.alert("Thank You for Playing!");
+		
+		if (!document.getElementById("linkd")) {
+			let linkd = document.createElement("a");
+			document.body.appendChild(link);
+			linkd.href = "https://hotdoguy90.github.io/";
+			linkd.innerText = "Click here just in case you want to go back to the homepage.";
+			linkd.id = "linkd";
+		}
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
